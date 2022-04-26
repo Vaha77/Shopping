6a1c82ff026f3092de44316801f79e630ac8029b
vaha77 <abdulvohid996@gmail.com>
2022-04-26 16:11:24 +0900

    xs

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,13 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import { Product } from "../../components";
 import { client, urlFor } from "../../lib/client";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
 const ProdactDetails = ({ products, product }) => {
-  const { image, name, details, price } = product;
+  const { image, title, name, details, price } = product;
+  const [index, setIndex] = useState(0);
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} />
+            <img
+              className="product-detail-image"
+              src={urlFor(title && title[index])}
+            />
+          </div>
+          <div className="small-images-container">
+            {title?.map((item, i) => (
+              <img
+                src={urlFor(item)}
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details:</h4>
+          <p>{details}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick="">
+                <AiOutlineMinus />{" "}
+              </span>
+              <span className="num" onClick="">
+                0
+              </span>
+              <span className="plus" onClick="">
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+
+          <div className="buttons">
+            <button type="button" className="add-to-cart" onClick="">
+              Add to Cart
+            </button>
+            <button type="button" className="buy-now" onClick="">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You my also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
           </div>
         </div>
       </div>
@@ -15,13 +87,15 @@ const ProdactDetails = ({ products, product }) => {
   );
 };
 export const getStaticPaths = async () => {
-  const query = `*[_type == "product"]{
-        slug{
-            current
-        }
+  const query = `*[_type == "product"] {
+      slug {
+        current
+      }
     }
     `;
+
   const products = await client.fetch(query);
+
   const paths = products.map((product) => ({
     params: {
       slug: product.slug.current,
@@ -34,13 +108,15 @@ export const getStaticPaths = async () => {
   };
 };
 
-export const getstaticProps = async ({ params: { slug } }) => {
-  const query = `*[_type == "product"&& slug.current == '${slug}][0]`;
-  const productsQuery = `*[_type == "product"]`;
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const productsQuery = '*[_type == "product"]';
 
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
+
   console.log(product);
+
   return {
     props: { products, product },
   };
