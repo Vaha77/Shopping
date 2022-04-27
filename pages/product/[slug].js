142908633ae6246ddb272324cd72b730445b344c
vaha77 <abdulvohid996@gmail.com>
2022-04-27 13:23:04 +0900

    vaha

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,9 +7,13 @@ import {
   AiFillStar,
   AiOutlineStar,
 } from "react-icons/ai";
+
+import { useStateContext } from "../../context/StateContext";
 const ProdactDetails = ({ products, product }) => {
   const { image, title, name, details, price } = product;
   const [index, setIndex] = useState(0);
+  // -+
+  const { decQty, incQty, qty, onAdd } = useStateContext();
   return (
     <div>
       <div className="product-detail-container">
@@ -51,20 +55,24 @@ const ProdactDetails = ({ products, product }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus" onClick="">
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />{" "}
               </span>
               <span className="num" onClick="">
-                0
+                {qty}
               </span>
-              <span className="plus" onClick="">
+              <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
             </p>
           </div>
 
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick="">
+            <button
+              type="button"
+              className="add-to-cart"
+              onClick={() => onAdd(product, qty)}
+            >
               Add to Cart
             </button>
             <button type="button" className="buy-now" onClick="">
